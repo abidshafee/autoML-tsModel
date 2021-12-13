64de625dd7c135f9e5bf44f378ab0b8e57eb2050
Shafekul Abid <abidshafee@gmail.com>
2021-12-13 19:38:13 +0600

    state update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from os import name
 import streamlit as st
 # import auto_ts as ts
 import pandas as pd
+import statsmodels
 from sklearn.preprocessing import StandardScaler
 import plotly
 import plotly.express as px
@@ -13,12 +14,12 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 from datetime import datetime
 
-st.title("Throughput Metrics")
+st.title("Throughput Metrics Forcasting")
 
 @st.cache(allow_output_mutation=True)
 def load_dataset():
     data = pd.read_csv("https://raw.githubusercontent.com/abidshafee/autoML-tsModel/main/throughput_metrics.csv", parse_dates=['Time'], index_col='Time')
-    data = data.dropna()
+    data = data.dropna(inplace=True)
     return data
 
 df = load_dataset()
@@ -32,26 +33,26 @@ st.write(df.describe(include='all'))
 # st.pyplot()
 # st.line_chart(df)
 
+
 fig = make_subplots(rows=6, cols=1,
                     shared_xaxes=True,
                     vertical_spacing=0.03)
 
-fig.add_trace(go.Scatter(x=df.index, y=df['SiteA'], name="SiteA"),
-              row=6, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=df['SiteB'], name="SiteB"),
-              row=5, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=df['SiteC'], name="SiteC"),
-              row=4, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=df['SiteD'], name="SiteD"),
-              row=3, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=df['SiteE'], name="SiteE"),
-              row=2, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=df['SiteF'], name="SiteF"),
-              row=1, col=1)
-fig.update_layout(height=600, width=800, xaxis6_rangeslider_visible=True,
-                  title_text="Stacked All Sites Data", showlegend=True,
-                  legend_orientation="v",
-                  xaxis6_rangeslider_thickness=0.1)
+# for i, col in enumerate(df.columns[0:], 1):
+# for i, col in enumerate(df.columns[0:]):
+    # fig.add_trace(go.Scatter(x=df.index, y=df[col], name=df.columns[i]), row=6, col=1)
+
+fig.add_trace(go.Scatter(x=df.index, y=df['SiteA'], name="SiteA"), row=6, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=df['SiteB'], name="SiteB"), row=5, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=df['SiteC'], name="SiteC"), row=4, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=df['SiteD'], name="SiteD"), row=3, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=df['SiteE'], name="SiteE"), row=2, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=df['SiteF'], name="SiteF"), row=1, col=1)
+
+#fig = stack_plot()
+fig.update_layout(height=680, width=800, xaxis6_rangeslider_visible=True,
+                  title_text="Stacked Sites Data", showlegend=True,
+                  xaxis6_rangeslider_thickness=0.05)
 st.plotly_chart(fig, use_container_width=True)
 
 # Plotting all features together
@@ -71,6 +72,7 @@ fig1.update_layout(title_text='Individual Site Data')
 # fig1.update_xaxes(rangeslider_visible=True)
 fig1.update_xaxes(
     rangeslider_visible=True,
+    rangeslider_thickness=0.05,
     rangeselector=dict(
         buttons=list([
             dict(count=1, label="1d", step="day", stepmode="backward"),
